139d14ab2c77c6ef0f104ad199de456ac4dbde57
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2025-04-18 19:54:53 +0530

    Fix asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/react-web-hosting/', // 👈 use the exact repo name here!
   plugins: [react()],
+  base: '/react-web-hosting/', // <-- IMPORTANT
 })
+
